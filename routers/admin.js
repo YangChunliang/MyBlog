4be918796933612858ef6617242efc8d0f8d96b2
YangChunliang <1711626079@qq.com>
2017-05-27 15:27:38 +0800

    后台用户数据管理

--- routers/admin.js
+++ routers/admin.js
@@ -1,5 +1,6 @@
 const express = require('express');
 var router = express.Router();
+var User = require('../models/User');
 
 router.use(function(req,res,next){
   if (!req.userInfo.isAdmin) {
@@ -13,6 +14,32 @@ router.get('/',function(req,res,next){
   res.render('admin/index',{
     userInfo: req.userInfo
   });
+})
+
+router.get('/user',function(req,res,next){
+  // 从数据库中读取数据
+
+  var page = Number(req.query.page) || 1;//初始页
+  var limit = 2;//限制条数
+  var pages;//总页数
+  User.count().then(function(count){//返回User实例的总条数
+    //计算总页数
+    pages = Math.ceil(count/limit);
+    page = Math.min(page,pages);
+    page = Math.max(page,1);
+    //跳过的条数
+    var skip = (page -1)*limit;
+
+    User.find().limit(limit).skip(skip).then(function(users){
+      res.render('admin/user_index',{
+        userInfo: req.userInfo,
+        users: users,
+        page: page,
+        pages: pages
+      });
+    });
+  })
+
 })
 
 module.exports = router;

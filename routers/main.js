6c1d3ec11b8917015e630b05dbf35c14b2078796
YangChunliang <1711626079@qq.com>
2017-05-30 22:33:09 +0800

    前台内容分页展示

--- routers/main.js
+++ routers/main.js
@@ -1,15 +1,36 @@
 const express = require('express');
 var router = express.Router();
 var Category = require('../models/Category');
+var Content = require('../models/Content');
 
 router.get('/',function(req,res,next){
-
+  var data = {
+    userInfo: req.userInfo,
+    categories: [],
+    count:0,
+    page:Number(req.query.page) || 1,//初始页
+    limit:4,//限制条数
+    pages:0//总页数
+  }
   //读取所有的分类信息
   Category.find().then(function(categories){
-    res.render('main/index',{
-      userInfo: req.userInfo,
-      categories: categories
-    });
+
+    data.categories = categories;
+    return Content.count();
+  }).then(function(count){
+    data.count = count;
+    //计算总页数
+    data.pages = Math.ceil(data.count/data.limit);
+    data.page = Math.min(data.page,data.pages);
+    data.page = Math.max(data.page,1);
+    //跳过的条数
+    var skip = (data.page -1)*data.limit;
+    return Content.find().sort({
+      addTime: -1
+    }).limit(data.limit).skip(skip).populate(['category','user']);
+  }).then(function(contents){
+    data.contents = contents;
+    res.render('main/index',data);
   });
 
 });

c805d062d6ef07f729be40f9c840ec146f1c1b4f
YangChunliang <1711626079@qq.com>
2017-05-31 11:58:57 +0800

    内容详情页与阅读数

--- routers/main.js
+++ routers/main.js
@@ -2,29 +2,34 @@ const express = require('express');
 var router = express.Router();
 var Category = require('../models/Category');
 var Content = require('../models/Content');
-
-router.get('/',function(req,res,next){
-  var data = {
+var data;
+//处理通用数据
+router.use(function(req,res,next){
+  data = {
     userInfo: req.userInfo,
-    category: req.query.category,
-    categories: [],
-    count:0,
-    page:Number(req.query.page) || 1,//初始页
-    limit:4,//限制条数
-    pages:0//总页数
+    categories: []
   }
 
+  Category.find().then(function(categories){
+    data.categories = categories;
+    next();
+  })
+});
+
+router.get('/',function(req,res,next){
+
+  data.category = req.query.category || '';
+  data.count = 0;
+  data.page = Number(req.query.page) || 1;
+  data.limit = 4;
+  data.pages = 0;
   //读取所有的分类信息
   var where = {};
   if (data.category) {
     where.category = data.category;
   }
 
-  Category.find().then(function(categories){
-
-    data.categories = categories;
-    return Content.where(where).count();
-  }).then(function(count){
+  Content.where(where).count().then(function(count){
     data.count = count;
     //计算总页数
     data.pages = Math.ceil(data.count/data.limit);
@@ -36,6 +41,22 @@ router.get('/',function(req,res,next){
   }).then(function(contents){
     data.contents = contents;
     res.render('main/index',data);
+  });
+
+});
+
+router.get('/view',function(req,res){
+  var contentId = req.query.contentid || '';
+
+  Content.findOne({
+    _id: contentId
+  }).then(function(content){
+    data.content = content;
+
+    content.views++;
+    content.save();
+
+    res.render('main/view',data);
   });
 
 });
